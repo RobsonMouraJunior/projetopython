a91cfcbfb1291cec1bea8281140862da2cac06df
RobsonMouraJunior <dutramourar@gmail.com>
2024-04-12 21:22:12 -0300

    hello world

---
+++ aula01_primeiro_pograma/primeiro_projeto.py
@@ -0,0 +1,19 @@
+#curso de Desenvolvimento de Sistema
+#Turma 0152 (braba)
+#Autor: Robson Moura  
+#Data: 12/04/2024
+#primeiro programa em python
+
+#Importando as bibliotecas 
+import os 
+
+
+#Limpando o terminal 
+os.system('cls')
+
+# O comando priny() executa uma saía via terminal 
+
+print('-' * 70)
+print('Hello world!!!')
+print('-' * 70)
+print()
