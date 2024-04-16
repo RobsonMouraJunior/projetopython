f9ea48148e9dfb7975dbf10868a3eef8329fe068
RobsonMouraJunior <dutramourar@gmail.com>
2024-04-16 19:59:22 -0300

    entrada de dados

--- aula02_variaveis/variaveis.py
+++ aula02_variaveis/variaveis.py
@@ -18,7 +18,7 @@ doador = True
 complexo = 3j # python trabalha diretamente  com numeros complexos
 PI = 3.14 # isso e uma CONSTANTE, seu valor nao deve ser alterado
 
-#Saida utilizando o metodo type() para verificar o ______________________________
+#Saida utilizando o metodo type() para verificar o ________________________________________________
 #tipo da variavel 
 print('\033[0m \033[31mtipos declarados;\033[0m')
 print('\033[0m A var \033[32m nome \033[0m é do tipo: ', type(nome))
@@ -26,7 +26,7 @@ print('\033[0m A var \033[32m nascimento \033[0m é do tipo: ', type(nascimento)
 print('\033[0m A var \033[32m altura \033[0m é do tipo: ', type(altura))
 print('\033[0m A var \033[32m peso \033[0m é do tipo: ', type(peso))
 print('\033[0m A var \033[32m doador\033[0m é do tipo: ', type(doador))
-print('\033[0m A var \033[32m complexo \033[0m é do tipo: ', type(ncomplexo))
+print('\033[0m A var \033[32m complexo \033[0m é do tipo: ', type(complexo))
 print('\033[0m A var \033[32m PI\033[0m é do tipo: ', type(PI))
 print('-'*70)
 print('')
---
+++ aula03_entrada_de_dados/entrada_de_dados.py
@@ -0,0 +1,27 @@
+#imporatando a biblioteca
+import os
+
+
+#limapando terminal
+os.system('cls')
+
+print('-'*70)
+print('Estudos de variaveis')
+print('='*70)
+
+#entrada
+nome = input('Robson: ')
+nascimento = ('07/05/2006: ')
+peso = ('80:')
+altura = ('1.85: ')
+
+#saida________________________________________________________________________
+print('-'*70)
+print ('saida de dados')
+print('='*70)
+print('nome...............: ', nome)
+print('data de nascimento...............: ', nascimento)
+print('peso...............: ', peso)
+print('altura...............: ', altura)
+print('-'*70)
+print('')
