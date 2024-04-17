db04b9128ec01bd6704dc87b46e069f9471a716d
RobsonMouraJunior <dutramourar@gmail.com>
2024-04-16 21:19:18 -0300

    string

--- aula03_Template_String/Template_String.py
+++ aula03_Template_String/Template_String.py
@@ -13,16 +13,16 @@ print('saida de dados')
 print('='*70)
 
 #entrada
-nome = input('entre com um nome: ')
-nome = input(' entre com o peso: ')
-nome = input('Entre com a altura: ')
+nome = input('Entre com  onome')
+peso = input('Entre com o peso: ')
+altura= input('Entre com a altura: ')
 
 #Entrada com Casting
-nascimento = int(input('data de nascimento: '))
+nascimento = int(input( 'data de nascimento'))
 cep = int(input('Entre com o seu CEP: '))
-bairro = int(input('entre com o bairro: '))
-rua = int(input('nome da rua: '))
-nnumero = int(input('Entre com o numero: '))
+bairro = int(input('Entre com o bairro: '))
+rua = int(input('Entre com a rua: '))
+numero = int(input('Entre com o numero: '))
 
 #Processamento: pegando o ano corrente
 ano_atual = datetime.datetime.now().year
@@ -32,16 +32,16 @@ idade = int(ano_atual) - nascimento
 print('-'*70)
 print('saida de dados')
 print('='*70)
-print('nome...............: ', nome)
-print('data de nascimento...............: ', 2006)
-print('peso...............: ',80)
-print('altura...............: ', 1.85)
+print('nome...............: ',  nome)
+print('data de nascimento...............: ',nascimento)
+print('peso...............: ',peso)
+print('altura...............: ', altura)
 
 #Saida Intrpolada 
 print(f'{nome}, você tem {idade} anos!')
 print(f'cep...............: {cep}')
 print(f'bairro...............: {bairro}')
 print(f'rua...............:  {rua}')
-print(f'Numero...............:  {nnumero}')
+print(f'Numero...............:  {numero}')
 print('-'*70)
 print('')
