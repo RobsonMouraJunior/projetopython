607e62432a1a530c911df4466922f0977018b334
RobsonMouraJunior <dutramourar@gmail.com>
2024-04-19 19:42:56 -0300

    atividade 2

---
+++ atividade_02/j).py
@@ -0,0 +1,14 @@
+# import
+# biblioteca para interagir o SO
+import os
+
+
+# limapando terminal
+os.system('cls')
+
+print('-'*70)
+print('saida de dados')
+print('='*70)
+
+entre_com_tamanho_da_base_1 = float(input('Entre com o tamanho da base:'))
+entre_com_tamanho_da_altura_= float(input('Entre com o tamanho da altura  '))
