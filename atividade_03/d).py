584d2c7cda1ab7ba3d924d4c914e004f95415293
RobsonMouraJunior <dutramourar@gmail.com>
2024-04-26 21:31:59 -0300

    exercicio 4

---
+++ atividade_03/d).py
@@ -0,0 +1,11 @@
+#curso de Desenvolvimento de Sistema
+#Turma 0152 (braba)
+#Autor: Robson Moura  
+#Data: 24/04/2024
+
+#Importando as bibliotecas 
+import os 
+
+
+#Limpando o terminal 
+os.system('cls')
