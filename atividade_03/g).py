7ae1618c4761d0ea9bcf108f699d3b08f3c8bda2
RobsonMouraJunior <dutramourar@gmail.com>
2024-04-28 09:30:20 -0300

    atividade_3

---
+++ atividade_03/g).py
@@ -0,0 +1,32 @@
+#curso de Desenvolvimento de Sistema
+#Turma 0152 (braba)
+#Autor: Robson Moura  
+#Data: 24/04/2024
+
+#Importando as bibliotecas 
+import os 
+
+
+#Limpando o terminal 
+os.system('cls')
+print('-'*70)
+print('Estudo de Condicional: Operadores lógicos')
+print('='*70)
+
+#Entrada
+seg_1 = float(input('Digite a médida de um segmento de triangulo:'))
+seg_2 = float(input('Digite a médida de um segmento de triangulo:'))
+seg_3 = float(input('Digite a médida de um segmento de triangulo:'))
+resposta = ''
+
+#proceso
+if  seg_1 + seg_2 > seg_3 and seg_2 + seg_3 > seg_1 and seg_1 + seg_3 > seg_2:
+    print('Os 3 segmentos formam um triangulo')
+else:
+    print('Os segmentos não formam um triangulo')
+
+# Saída
+print('-'*70)
+print(resposta)
+print('='*70)
+print('Fim do programa!\n')
---
+++ atividade_03/h).py
@@ -0,0 +1,29 @@
+#curso de Desenvolvimento de Sistema
+#Turma 0152 (braba)
+#Autor: Robson Moura  
+#Data: 24/04/2024
+
+#Importando as bibliotecas 
+import os 
+
+
+#Limpando o terminal 
+os.system('cls')
+print('-'*70)
+print('Estudo de Condicional: Operadores lógicos')
+print('='*70)
+
+
+# entrada_Coeficientes da equação quadrática ax^2 + bx + c
+a = 1
+b = -6
+c = 5
+
+# Calcula as raízes usando a fórmula de Bhaskara
+x1 = (b + (b**2 - 4*a*c)**0.5) / (2*a)
+x2 = (b - (b**2 - 4*a*c)**0.5) / (2*a)
+
+# Imprime as raízes calculadas
+print("As raízes da equação quadrática são:")
+print("x' =", x1)
+print("x'' =", x2)
