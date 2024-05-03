a73a77e85b04ddf0dcc84e81a85e4e7cdd8f35c1
RobsonMouraJunior <dutramourar@gmail.com>
2024-05-02 21:29:02 -0300

    teste

--- teste/teste.py
+++ teste/teste.py
@@ -23,19 +23,29 @@ def escrever_beatriz():
     tartaruga = turtle.Turtle()
 
     tartaruga.penup()
-    tartaruga.goto(0, 100)
+    tartaruga.goto(-150, 100)  # Posição inicial para o texto "Beatriz"
     tartaruga.pendown()
 
     tartaruga.color('black')
-
-    # Função para animar o texto
-    def animar_texto():
-        tartaruga.write("Beatriz", align="center", font=("Arial", 20, "normal"))
-
-    # Agendar a animação do texto após um curto atraso
-    turtle.ontimer(animar_texto, 1000)
-
     tartaruga.hideturtle()
+
+  # Texto a ser escrito
+texto = "Beatriz"
+    # Configurações da fonte
+fonte = ("Arial", 60, "normal")
+    # Espaçamento entre letras
+espaço_letra = 20
+
+    # Função para escrever uma letra do texto "Beatriz"
+def escrever_letra(letra_idx):
+        if letra_idx < len(texto):
+            # Escreve a próxima letra do texto
+         tartaruga.write(texto[letra_idx], align="left", font = fonte)
+            # Agendando a escrita da próxima letra com um pequeno atraso
+        turtle.ontimer(lambda: escrever_letra(letra_idx + 1), 300)  # Ajuste o tempo conforme desejado
+
+    # Inicia a escrita da primeira letra
+escrever_letra(0)
 
 desenhar_coracao()
 escrever_beatriz()
